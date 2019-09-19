afeecfd67adb95482df686dbdd1dde1716cd176d
Lee Vanrell <leegvanrell@gmail.com>
2019-09-19 17:52:23 -0500

    added ssh config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
+set -e
+
 FOLDER_PATH=$(PWD)
 
 sudo apt -y update && sudo apt -y upgrade
@@ -12,7 +14,7 @@ sudo apt -y update && sudo apt -y upgrade
 #### python ####
 
 sudo apt install -y python3 python3-pip
-sudo pip install pipenv
+sudo pip3 install pipenv
 
 #### terminal emulator #### 
 
@@ -39,7 +41,6 @@ cp .zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-
 #### editor #### 
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -47,6 +48,18 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt update -y
 sudo apt install sublime-text
 sudo python -c """import urllib.request,os,hashlib; h = '6f4c264a24d933ce70df5dedcf1dcaee' + 'ebe013ee18cced0ef93d5f746d80ef60'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)"""
+
+#### SSH ####
+
+sudo apt install -y ssh
+sudo tee -a /etc/ssh/sshd_config > /dev/null <<EOT
+PORT 372
+X11Forwarding yes
+X11DisplayOffset 10
+X11UseLocalhost yes
+EOT
+sudo systemctl enable ssh
+sudo systemctl start ssh
 
 #### desktop ####
 
